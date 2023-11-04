a16f360c6f3a0898a3265c200a1c2dbb88b06e96
Bilal Ahmad <98654652+bilalahmad3013@users.noreply.github.com>
2023-11-04 17:03:48 +0530

    Update Messages.jsx

--- src/Screens/Messages.jsx
+++ src/Screens/Messages.jsx
@@ -9,23 +9,8 @@ export default function Messages({ user }) {
 
   return (
     <div>
-     
-    </div>
-  );
-}
-=======
-import React, { useContext, useState, useEffect } from 'react';
-import { StatesProvider } from '../States/states';
-import socketIO from 'socket.io-client';
-
-export default function Messages({ user }) {
-  const { setTitle } = useContext(StatesProvider);
- 
-
-  return (
-    <div>
      hiiiii
     </div>
   );
 }
->>>>>>> f83fb0f83a177ffe9c9bfabba4a67835dba026f3
+
